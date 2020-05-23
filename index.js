0589f0008fe16333bac04a92f8dc6ae147c90dc6
JhonRams <64045380+JhonRams@users.noreply.github.com>
2020-05-23 00:21:26 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function mostrar_ventana(){
+    var div=document.getElementById("contacto");
+    div.style.display="block";
+}
+function ocultar_ventana(){
+    var div=document.getElementById("contacto");
+    div.style.display="none";
+}
+
